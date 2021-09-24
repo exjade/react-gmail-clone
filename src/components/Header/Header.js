6359c6b22df3b0aa49ce3a195ccfa5e775f1033a
Adrian Zamora <adrianzamora10@outlook.com>
2021-09-24 14:20:57 -0600

    left header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,23 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import { IconButton } from '@material-ui/core';
+import Menu from '@material-ui/icons/Menu';
 
 const Header = () => {
     return (
         <div className="header">
-            <h2>This is a Header</h2>            
+            <div className="header__left">
+                <IconButton>
+                    <Menu />
+                </IconButton>
+                <img src="https://clientes.rcymedia.eu/media/Gmail/d74e6036cb5b6e80d87b0a3fc77176c0.png" alt="mail logo" />
+            </div>
+            <div className="header__middle">
+
+            </div>
+            <div className="header__right">
+
+            </div>
         </div>
     )
 }
