3d8ed292dd0fda9ea8ca9954d5922f5e805bd207
Adrian Zamora <adrianzamora10@outlook.com>
2021-09-24 14:53:02 -0600

    middle header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './Header.css';
 import { IconButton } from '@material-ui/core';
 import Menu from '@material-ui/icons/Menu';
+import Search from '@material-ui/icons/Search';
+import ArrowDropDown from '@material-ui/icons/ArrowDropDown';
 
 const Header = () => {
     return (
@@ -13,7 +15,9 @@ const Header = () => {
                 <img src="https://clientes.rcymedia.eu/media/Gmail/d74e6036cb5b6e80d87b0a3fc77176c0.png" alt="mail logo" />
             </div>
             <div className="header__middle">
-
+                <Search />
+                <input type="text" placeholder="Search mail" />
+                <ArrowDropDown className="header_inputCaret"/>
             </div>
             <div className="header__right">
 
