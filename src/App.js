2d54351aadcbb0645c7ab2e377afa9b08bc16ed3
Adrian Zamora <adrianzamora10@outlook.com>
2021-09-24 13:40:39 -0600

    header init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header'
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Header />
      <h1>Let's build and app</h1>
     </div>
   );
