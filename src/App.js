db6e8915535d535bfeec330ab56e95d51494f465
Adrian Zamora <adrianzamora10@outlook.com>
2021-09-24 20:04:23 -0600

    routing my components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header'
 import Sidebar from './components/Sidebar/Sidebar'
+import EmailList from './components/EmailList/EmailList'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Sidebar />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+
+        <div className="app__body">
+          <Sidebar />
+          <Route path="/mail">
+
+          </Route>
+          <Route path="/">
+            <EmailList />
+          </Route>
+          <Switch>
+
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
