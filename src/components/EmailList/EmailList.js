ac0547e3815a227c9ee6b736ad72e7290f1ffc7e
Adrian Zamora <adrianzamora10@outlook.com>
2021-09-24 20:47:35 -0600

    icon right

--- src/components/EmailList/EmailList.js
+++ src/components/EmailList/EmailList.js
@@ -4,6 +4,11 @@ import { Checkbox, IconButton } from '@material-ui/core';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown'
 import RedoIcon from '@material-ui/icons/Redo'
 import MoreVertIcon from '@material-ui/icons/MoreVert'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
+import KeyboardHideIcon from '@material-ui/icons/KeyboardHide'
+import SettingsIcon from '@material-ui/icons/Settings'
+
 
 const EmailList = () => {
     return (
@@ -19,6 +24,20 @@ const EmailList = () => {
                     </IconButton>
                     <IconButton>
                         <MoreVertIcon />
+                    </IconButton>
+                </div>
+                <div className="emailList__settingsRight">
+                    <IconButton>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChevronRightIcon />
+                    </IconButton>
+                    <IconButton>
+                        <KeyboardHideIcon />
+                    </IconButton>
+                    <IconButton>
+                        <SettingsIcon />
                     </IconButton>
                 </div>
             </div>
