7e05799f77534c94c426d90e9cf56453bb06d9e4
Adrian Zamora <adrianzamora10@outlook.com>
2021-09-24 19:52:59 -0600

    sidebar footer

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Sidebar.css';
 import SidebarOptionList from './SidebarOptionList/SidebarOptionList';
-import { Button } from '@material-ui/core';
+import { Button, IconButton } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import InboxIcon from '@material-ui/icons/Inbox';
 import StarIcon from '@material-ui/icons/Star';
@@ -11,6 +11,9 @@ import SendIcon from '@material-ui/icons/Send';
 import LabelIcon from '@material-ui/icons/Label';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
+import PersonIcon from '@material-ui/icons/Person';
+import PhoneIcon from '@material-ui/icons/Phone';
+import DuoIcon from '@material-ui/icons/Duo';
 
 const Sidebar = () => {
     return (
@@ -29,45 +32,57 @@ const Sidebar = () => {
             <SidebarOptionList
                 Icon={StarIcon}
                 title="Starred"
-                number={130} 
+                number={130}
             />
             <SidebarOptionList
                 Icon={AccessTimeIcon}
                 title="Snoozed"
-                number={50} 
+                number={50}
             />
             <SidebarOptionList
                 Icon={LabelImportantIcon}
                 title="Important"
-                number={35} 
+                number={35}
             />
             <SidebarOptionList
                 Icon={SendIcon}
                 title="Sent"
-                number={15} 
+                number={15}
             />
             <SidebarOptionList
                 Icon={InsertDriveFileIcon}
                 title="Drafts"
-                number={40} 
+                number={40}
             />
             <SidebarOptionList
                 Icon={LabelIcon}
                 title="Categories"
-                number={10} 
+                number={10}
             />
             <SidebarOptionList
                 Icon={LabelIcon}
                 title="To do"
-                number={0} 
+                number={0}
             />
             <SidebarOptionList
                 Icon={KeyboardArrowDownIcon}
                 title="More"
-                number={0} 
+                number={0}
             />
 
-
+            <div className="sidebar__footer">
+                <div className="sidebar__footerIcons">
+                <IconButton>
+                    <PersonIcon />
+                </IconButton>
+                <IconButton>
+                    <DuoIcon />
+                </IconButton> 
+                <IconButton>
+                    <PhoneIcon />
+                </IconButton>
+                </div>
+            </div>
         </div>
     )
 }
