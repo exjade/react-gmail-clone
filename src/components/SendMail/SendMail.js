b1d3c32311a890d26b70e73bf6b41d10635f86f9
Adrian Zamora <adrianzamora10@outlook.com>
2021-09-25 09:26:40 -0600

    Send Mail Component

--- src/components/SendMail/SendMail.js
+++ src/components/SendMail/SendMail.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Button } from '@material-ui/core'
+import CloseIcon from '@material-ui/icons/Close'
 import './SendMail.css'
 
 const SendMail = () => {
     return (
-        <div>
-            
+        <div className="sendMail">
+            <div className="sendMail__header">
+                <h3>New Message</h3>
+                <CloseIcon className="sendMail__close" />
+            </div>
+
+            <form>
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+
+                <div className="sendMail__options">
+                    <Button 
+                    className="sendMail__send"
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    >Send</Button>
+                </div>
+            </form>
         </div>
     )
 }
