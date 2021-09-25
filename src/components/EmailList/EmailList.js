2668e494e56ffbd527763e73f80ee46afdb18fe4
Adrian Zamora <adrianzamora10@outlook.com>
2021-09-24 20:30:25 -0600

    icons added

--- src/components/EmailList/EmailList.js
+++ src/components/EmailList/EmailList.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import './EmailList.css'
+import { Checkbox, IconButton } from '@material-ui/core';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown'
+import RedoIcon from '@material-ui/icons/Redo'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
 
 const EmailList = () => {
     return (
-         <div className="emailList">
-             
+        <div className="emailList">
+            <div className="emailList__settings">
+                <div className="emailList__settingsLeft">
+                    <Checkbox />
+                    <IconButton>
+                        <ArrowDropDownIcon />
+                    </IconButton>
+                    <IconButton>
+                        <RedoIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
         </div>
     )
 }
