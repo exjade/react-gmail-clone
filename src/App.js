c07e4e0c319e36eac54bcf0826ce7175f9ced9fb
Adrian Zamora <adrianzamora10@outlook.com>
2021-09-26 04:01:41 -0600

    rendering loading screen

--- src/App.js
+++ src/App.js
@@ -1,35 +1,45 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header/Header'
-import Sidebar from './components/Sidebar/Sidebar'
-import EmailList from './components/EmailList/EmailList'
-import Email from './components/Mail/Email'
-import SendMail from './components/SendMail/SendMail'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { selectSendMessageIsOpen } from './features/mailSlice'
+import EmailList from './components/EmailList/EmailList';
+import Header from './components/Header/Header';
+import Email from './components/Mail/Email';
+import SendMail from './components/SendMail/SendMail';
+import Sidebar from './components/Sidebar/Sidebar';
+import { selectSendMessageIsOpen } from './features/mailSlice';
+import { selectUser } from './features/userSlice';
 
 function App() {
 
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen)
 
+  const user = useSelector(selectUser)
+
   return (
     <Router>
-      <div className="app">
-        <Header />
-        <div className="app__body">
-          <Sidebar />
-          <Switch>
-            <Route path="/mail">
-              <Email />
-            </Route>
-            <Route path="/">
-              <EmailList />
-            </Route>
-          </Switch>
-        </div>
-        {sendMessageIsOpen && <SendMail />}
-      </div>
+      {
+        !user ?
+          (<Login />)
+          :
+          (
+            <div className="app">
+              <Header />
+              <div className="app__body">
+                <Sidebar />
+                <Switch>
+                  <Route path="/mail">
+                    <Email />
+                  </Route>
+                  <Route path="/">
+                    <EmailList />
+                  </Route>
+                </Switch>
+              </div>
+              {sendMessageIsOpen && <SendMail />}
+            </div>
+          )
+      }
     </Router>
   );
 }
