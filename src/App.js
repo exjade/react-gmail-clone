60121697168fe0e82374677d0717b58431ee141b
Adrian Zamora <adrianzamora10@outlook.com>
2021-09-26 04:19:09 -0600

    login component imported

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import EmailList from './components/EmailList/EmailList';
 import Header from './components/Header/Header';
+import Login from './components/Login/Login';
 import Email from './components/Mail/Email';
 import SendMail from './components/SendMail/SendMail';
 import Sidebar from './components/Sidebar/Sidebar';
