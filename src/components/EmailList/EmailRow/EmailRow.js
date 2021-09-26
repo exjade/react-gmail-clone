60db3e9dbf368008ac2eef8cad0736d9297d6234
Adrian Zamora <adrianzamora10@outlook.com>
2021-09-26 04:13:38 -0600

    dispatch in an onclick event to set status to true, route change

--- src/components/EmailList/EmailRow/EmailRow.js
+++ src/components/EmailList/EmailRow/EmailRow.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import { useHistory } from 'react-router'
 import { Checkbox, IconButton } from '@material-ui/core'
+import { selectMail } from './../../../features/mailSlice'
+import { useDispatch } from 'react-redux'
 import StarOutlineIcon from '@material-ui/icons/StarOutline'
 import LabelImportantIcon from '@material-ui/icons/LabelImportant'
 import './EmailRow.css'
 
 const EmailRow = ({ id, title, subject, description, time }) => {
 
-    const history = useHistory()
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    const openMail = () => {
+        dispatch(selectMail({
+            id,
+            title,
+            subject,
+            description,
+            time
+        }));
+        history.push('/mail')
+    };
 
     return (
-        <div onClick={() => history.push("/mail")} className="emailRow">
+        <div onClick={openMail} className="emailRow">
             <div className="emailRow__options">
                 <Checkbox />
                 <IconButton>
@@ -24,9 +38,9 @@ const EmailRow = ({ id, title, subject, description, time }) => {
                 {title}
             </h3>
             <div className="emailRow__message">
-                <h4>{subject} 
-                <span className="emailRow__description">- {description}
-                </span>
+                <h4>{subject}
+                    <span className="emailRow__description">- {description}
+                    </span>
                 </h4>
             </div>
             <p className="emailRow__time">{time}</p>
