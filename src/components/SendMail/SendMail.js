e59b9ebdd1f0f1392fa747fa959e1b8152e3c457
Adrian Zamora <adrianzamora10@outlook.com>
2021-09-26 04:11:26 -0600

    form validation, I added a dispatch in an onclick event to set the state to false

--- src/components/SendMail/SendMail.js
+++ src/components/SendMail/SendMail.js
@@ -1,27 +1,38 @@
-import React from 'react'
-import CloseIcon from '@material-ui/icons/Close'
-import { Button } from '@material-ui/core'
-import { useForm } from 'react-hook-form'
-import { useDispatch } from 'react-redux'
+import React from 'react';
+import './SendMail.css';
+import CloseIcon from '@material-ui/icons/Close';
+import { Button } from '@material-ui/core';
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
 import { closeSendMessage } from '../../features/mailSlice';
-import './SendMail.css'
+import { db } from '../../firebase';
+import firebase from 'firebase/compat/app'
 
 const SendMail = () => {
 
-    const { register, handleSubmit, watch,  formState: { errors } } = useForm();
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const dispatch = useDispatch()
 
     const onSubmit = (formData) => {
         console.log(formData)
-    }
+        db.collection('emails').add(
+            {
+                to: formData.to,
+                subject: formData.subject,
+                message: formData.message,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp()
+            }
+        );
+        dispatch(closeSendMessage());
+    };
 
     return (
         <div className="sendMail">
             <div className="sendMail__header">
                 <h3>New Message</h3>
-                <CloseIcon 
-                className="sendMail__close" 
-                onClick={() => dispatch(closeSendMessage())}
+                <CloseIcon
+                    className="sendMail__close"
+                    onClick={() => dispatch(closeSendMessage())}
                 />
             </div>
 
@@ -31,22 +42,22 @@ const SendMail = () => {
                     placeholder="To"
                     type="email"
                     {...register("to", { required: true })}
-                     />
-                    {errors.to && <p className="sendMail__error">Specify at least one recipient.</p>}
+                />
+                {errors.to && <p className="sendMail__error">Specify at least one recipient.</p>}
                 <input
                     name="subject"
                     placeholder="Subject"
                     type="text"
-                    {...register("subjectRequired", { required: true })}
-                     />
-                      {errors.subjectRequired && <p className="sendMail__error">You must specify at least one subject</p>}
+                    {...register("subject", { required: true })}
+                />
+                {errors.subject && <p className="sendMail__error">You must specify at least one subject</p>}
                 <input
                     name="message"
                     type="text"
                     className="sendMail__message"
-                    {...register("messageRequired", { required: true })}
-                     />
-                      {errors.messageRequired && <p className="sendMail__error">This field can't be empty</p>}
+                    {...register("message", { required: true })}
+                />
+                {errors.message && <p className="sendMail__error">This field can't be empty</p>}
 
                 <div className="sendMail__options">
                     <Button
